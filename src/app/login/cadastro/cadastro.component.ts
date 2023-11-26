1ecdcd3354c25c8bc4d94688ebe3acd4c17ec8c5
Joel Neto <joelalvesneto187@gmail.com>
2023-11-26 13:49:40 -0300

    Sla

--- src/app/login/cadastro/cadastro.component.ts
+++ src/app/login/cadastro/cadastro.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Usuario } from 'src/app/services/usuarios/usuario';
 import { UsuarioService } from 'src/app/services/usuarios/usuario.service';
 
@@ -11,6 +12,7 @@ import { UsuarioService } from 'src/app/services/usuarios/usuario.service';
 
 export class CadastroComponent {
   'usuario': Usuario;
+  'codAdmin': string;
 
   constructor(private route: ActivatedRoute, private router: Router, private usuarioService: UsuarioService) {}
 
@@ -20,9 +22,33 @@ export class CadastroComponent {
   }
 
   OnSubmit() {
-    // const eAdmin = document.getElementById('input-cod-admin').value;
 
-    console.log("Submit: ", this.usuario)
+    let observable: Observable<Usuario>;
+    
+    if (!this.codAdmin || this.codAdmin !== "admin") {
+      this.usuario.eAdmin = false;
 
+      observable = this.usuarioService.insert(this.usuario);
+
+      observable.subscribe(() => {
+        this.router.navigate(['/main']);
+      })
+
+      return;
+    }
+
+    this.usuario.eAdmin = true;
+    observable = this.usuarioService.insert(this.usuario);
+
+    this.setSessionStorageValue();
+
+    observable.subscribe(() => {
+      this.router.navigate(['/admin']);
+    })
+
+  }
+
+  setSessionStorageValue(): void {
+    return sessionStorage.setItem('admin', 'true');
   }
 }
