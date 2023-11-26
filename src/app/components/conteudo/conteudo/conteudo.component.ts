1b6c99458e3ad1edee85fe424a689c88d708238d
alihpss <ali.hps@hotmail.com>
2023-11-26 11:21:26 -0300

    add admin

--- src/app/components/conteudo/conteudo/conteudo.component.ts
+++ src/app/components/conteudo/conteudo/conteudo.component.ts
@@ -10,16 +10,24 @@ import { ConteudosService } from 'src/app/materias/conteudos.service';
 export class ConteudoComponent {
 
   'conteudos': Conteudo[];
+  items: {id: number, title: string, course: string, eAdmin: boolean, content: string}[] = [];
 
-  constructor(private conteudoService: ConteudosService) {}
+  // constructor(private conteudoService: ConteudosService) {}
 
   ngOnInit () {
-    this.getAll();
+    // this.getAll();
+    this.items = [
+      {id: 1, title: "teste", course: "js", eAdmin: true, content: "conteudo"},
+      {id: 2, title: "teste 2 ", course: "php", eAdmin: true, content: "conteudo teste 2"},
+      {id: 3, title: "teste 3", course: "java", eAdmin: false, content: "conteudo teste 3"},
+      {id: 4, title: "teste 4", course: "css", eAdmin: false, content: "conteudo teste 4"},
+      {id: 5, title: "teste 5", course: "c#", eAdmin: true, content: "conteudo teste 5"},
+    ]
   }
 
-  getAll() {
-    this.conteudoService.getAll().subscribe(resp=> {
-      this.conteudos = resp;
-    })
-  }
+  // getAll() {
+  //   this.conteudoService.getAll().subscribe(resp=> {
+  //     this.conteudos = resp;
+  //   })
+  // }
 }
